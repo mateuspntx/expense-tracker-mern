9f3e39b80d5d47f0da471744e605f74a46f830db
mateuspntx <mateuspntx@gmail.com>
2021-01-08 15:39:09 -0300

    chore: add routes

--- server.js
+++ server.js
@@ -2,12 +2,23 @@ const express = require('express');
 const dotenv = require('dotenv');
 const colors = require('colors');
 const morgan = require('morgan');
+const connectDB = require('./config/db');
 
 dotenv.config({ path: './config/config.env' });
 
+connectDB();
+
+const transactions = require('./routes/transactions');
+
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello'));
+app.use(express.json());
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+app.use('/api/v1/transactions', transactions);
 
 const PORT = process.env.PORT || 5000;
 
